6a3ebabb0f813c7366e19bc154249b646233fdd7
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2025-03-27 11:57:42 +0100

    version 01 - GET /db/alive endpoint to check for database aliveness

---
+++ 01/app.py
@@ -0,0 +1,57 @@
+"""
+add a /db/alive endpoint to check if the database is alive
+"""
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import text
+
+VERSION = "01"
+
+## usual Flask initilization
+app = Flask(__name__)
+
+## DB declaration
+
+# filename where to store stuff (sqlite is file-based)
+db_name = 'chat.db'
+# how do we connect to the database ?
+# here we say it's by looking in a file named chat.db
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + db_name
+# this variable, db, will be used for all SQLAlchemy commands
+db = SQLAlchemy(app)
+
+
+@app.route('/')
+def hello_world():
+    return f'hello, this is a chat app! (version {VERSION})'
+
+
+# try it with
+"""
+http :5001/db/alive
+"""
+@app.route('/db/alive')
+def db_alive():
+    try:
+        result = db.session.execute(text('SELECT 1'))
+        print(result)
+        return dict(status="healthy", message="Database connection is alive")
+    except Exception as e:
+        # e holds description of the error
+        error_text = "<p>The error:<br>" + str(e) + "</p>"
+        hed = '<h1>Something is broken.</h1>'
+        return hed + error_text
+
+
+# try it with
+"""
+http :5001/api/version
+"""
+@app.route('/api/version')
+def version():
+    return dict(version=VERSION)
+
+
+if __name__ == '__main__':
+    app.run()

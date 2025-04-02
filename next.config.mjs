7a102131f7ab7a85d32d62dc9c0b34645902c200
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2025-04-02 14:43:06 +0200

    monorepo - inject codehike next app

---
+++ next.config.mjs
@@ -0,0 +1,30 @@
+import { remarkCodeHike, recmaCodeHike } from "codehike/mdx"
+import createMDX from "@next/mdx"
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",
+  // Configure `pageExtensions`` to include MDX files
+  pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
+  // Optionally, add any other Next.js config below
+  // when deploying on GitHub Pages we end up under
+  // https://ue22-p24.github.io/backend-flask-chatapp/
+  basePath: "/backend-flask-chatapp",
+}
+
+/** @type {import('codehike/mdx').CodeHikeConfig} */
+const chConfig = {
+  components: { code: "Code" },
+}
+
+const withMDX = createMDX({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [[remarkCodeHike, chConfig]],
+    recmaPlugins: [[recmaCodeHike, chConfig]],
+    jsx: true,
+  },
+})
+
+// Merge MDX config with Next.js config
+export default withMDX(nextConfig)
---
+++ readme.md
@@ -0,0 +1,17 @@
+# step by step chat app - in codehike
+
+## what is it ?
+
+this are the sources for a static website generated from the contents of
+
+https://github.com/ue22-p24/backend-flask-chatapp
+
+## where is it ?
+
+for starters the outcome is published on 
+
+https://backend-chatapp.surge.sh/scrollycoding
+
+## how is it done ?
+
+it uses [the fabulous codehike tool](https://codehike.org/) - and so [the unavoidable next.js](nextjs.org) underneath

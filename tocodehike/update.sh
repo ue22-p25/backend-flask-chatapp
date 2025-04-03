a207351c0d2ba818a50e86b1a5284cd51e65d033
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2025-04-03 19:55:47 +0200

    shorter output of update.sh

--- tocodehike/update.sh
+++ tocodehike/update.sh
@@ -2,17 +2,17 @@ BIN=$(dirname $(realpath $0))
 ROOT=$(dirname $BIN)
 APP=${ROOT}/app
 
-
+cd $ROOT/STEPS
 
 function redo-1() {
     local out=$APP/singlecolumn/AUTO
-    python $BIN/tocodehike.py chain-dirs $ROOT/STEPS/[0-9]* > $out
+    python $BIN/tocodehike.py chain-dirs [0-9]* > $out
     echo single column output written in $out
 }
 
 function redo-2() {
     local out=$APP/scrollycoding/AUTO
-    python $BIN/tocodehike.py chain-dirs -s $ROOT/STEPS/[0-9]* > $out
+    python $BIN/tocodehike.py chain-dirs -s [0-9]* > $out
     echo double column output written in $out
 }
 

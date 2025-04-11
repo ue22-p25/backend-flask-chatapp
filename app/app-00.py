5bb42ee51e5b8b9a6dd36e9f90be7daff3e8b79b
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2025-04-11 16:36:09 +0200

    add missing app/app-00.py

---
+++ app/app-00.py
@@ -0,0 +1,17 @@
+"""
+the simplest possible hello world app
+"""
+
+VERSION = "00"
+
+from flask import Flask
+
+## usual Flask initilization
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return f'hello, this is a chat app! (version {VERSION})'
+
+if __name__ == '__main__':
+    app.run()
